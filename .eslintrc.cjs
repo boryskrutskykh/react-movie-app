c7098c0693dd0f41ef80b0650ab5f7e7f00b8d0e
borys_krutskykh <borys_krutskykh@epam.com>
2024-03-07 22:38:11 +0200

    Add prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'no-unsafe-optional-chaining': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+};
