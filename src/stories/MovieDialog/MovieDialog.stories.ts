d623cfa327f10fd90b2957964aa5832517322e81
borys_krutskykh <borys_krutskykh@epam.com>
2024-03-20 01:50:09 +0200

    Add story

---
+++ src/stories/MovieDialog/MovieDialog.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MovieInfo, MovieTileProps } from '../../components/movie-tile/movie-tyle.types';
+import MovieDialog from '../../components/dialog/MovieDialog';
+
+const sampleMovieInfo: MovieInfo = {
+  imageUrl: 'https://example.com/movie-poster.jpg',
+  name: 'Inception',
+  releaseYear: '2010',
+  genres: ['Action', 'Adventure', 'Sci-Fi'],
+  rating: '8.8',
+  duration: '148 min',
+  description:
+    'A thief who steals corporate secrets through use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.',
+};
+
+const meta = {
+  title: 'MyStory/MovieDialog',
+  component: MovieDialog,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof MovieDialog>;
+
+export default meta;
+
+export const EditMovieDialog: StoryObj<typeof MovieDialog> = {
+  args: {
+    isOpen: true,
+    onClose: () => console.log('Dialog closed'),
+    initialMovieInfo: sampleMovieInfo,
+    onSubmit: (movieInfo) => console.log('Submitted movie info:', movieInfo),
+    title: 'Edit Movie',
+  },
+};
+
+export const AddNewMovieDialog: StoryObj<typeof MovieDialog> = {
+  args: {
+    isOpen: true,
+    onClose: () => console.log('Dialog closed'),
+    initialMovieInfo: undefined,
+    onSubmit: (movieInfo) => console.log('Submitted new movie info:', movieInfo),
+    title: 'Add New Movie',
+  },
+};
